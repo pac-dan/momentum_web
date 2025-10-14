e203d818067782e66208faf8fd25746460886237
Daniel Johnston <djjoshnton98@hotmail.com>
2025-10-14 21:16:11 +0100

    chore(vapi): single VAPI init, navbar button controls agent, hide floating widget, remove duplicates

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.get('/api/vapi-config', (req, res) => {
   res.setHeader('X-Content-Type-Options', 'nosniff');
   res.setHeader('X-Frame-Options', 'DENY');
   res.setHeader('X-XSS-Protection', '1; mode=block');
+  res.setHeader('Cache-Control', 'no-store');
   
   // Return VAPI configuration
   res.json({
